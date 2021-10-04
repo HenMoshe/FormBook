fd0a430dec319120beab48efc5f77c5ce232123a
HenMoshe <91905102+HenMoshe@users.noreply.github.com>
2021-10-04 19:57:57 +0300

    Forgot Env

--- DB.js
+++ DB.js
@@ -1,10 +1,10 @@
-///!DA4d5gJaVh*C.-    henadmin
+
 const mongoose = require('mongoose');
 require('dotenv').config()
 const dotenv = require('dotenv')
 const user = process.env.DB_USER
 const pass = process.env.DB_PASS
-const connectionstring = `mongodb+srv://henadmin:!DA4d5gJaVh*C.-@cluster0.7yafy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const connectionstring = `mongodb+srv://${user}:${pass}@cluster0.7yafy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 exports.connect = async function connect() {
   try {
     await mongoose.connect(connectionstring, {
