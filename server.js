e3487907d6bcb398cfa69398d318deef09ad1294
HenMoshe <91905102+HenMoshe@users.noreply.github.com>
2021-10-04 19:47:43 +0300

    Forgot Port.env

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express')
 const { connect } = require('./DB')
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
-const cors = require('cors')
 const path = require('path')
 const userRouter = require('./userRouter')
 const formRouter = require('./formRouter')
 const app = express()
-/* app.use(cors()) */
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'build')));
@@ -18,5 +16,5 @@ connect()
     .then(() => {
         userRouter(app)
         formRouter(app)
-        app.listen(3001)
-    })/* */
+        app.listen(3001 || process.env.PORT)
+    })
