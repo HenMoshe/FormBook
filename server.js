e10c78bc0fc46fdfad68f16d5b20036b3ec2e025
HenMoshe <91905102+HenMoshe@users.noreply.github.com>
2022-01-26 21:15:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'build')));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
+  next();
+});
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
